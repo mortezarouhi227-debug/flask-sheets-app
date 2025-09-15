22fc5754043d7cf90da7342e285fcfa13ccbcc06
Morteza Rouhi <mortezarouhi227@gmail.com>
2025-09-16 02:34:40 +0330

    Refactor: separate app.py and Performance_Segments.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+import Performance_Segments  # همون فایلی که بالا نوشتی
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello, Flask on Render! 🎉"
+
+@app.route("/update")
+def update_segments():
+    msg = Performance_Segments.main()
+    return msg
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
